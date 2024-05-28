e42f23ffcfb61629dd15f5e7b1c63eb442a643db
dnjawm19 <dnjawm19@naver.com>
2024-05-28 15:24:28 +0900

    feat: userService create,update 테스트코드 추가

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,16 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @TestPropertySource("classpath:test-application.properties")
@@ -23,6 +31,8 @@ public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @MockBean
+    private JavaMailSender mailSender;
 
     @Test
     void TestGetByEmail() {
@@ -50,5 +60,35 @@ public class UserServiceTest {
     @Test
     void TestGetByIdStatusPending() {
         assertThatThrownBy(() -> userService.getById(2)).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void TestCreateDto() {
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("dnjawm19@naver.com")
+                .address("경기도")
+                .nickname("무관심")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        UserEntity result = userService.create(userCreateDto);
+
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
+    }
+
+    @Test
+    void TestUpdateDto() {
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("서울")
+                .nickname("무관심하자")
+                .build();
+
+        userService.update(1,userUpdateDto);
+
+        UserEntity userEntity = userService.getById(1);
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getAddress()).isEqualTo("서울");
+        assertThat(userEntity.getNickname()).isEqualTo("무관심하자");
     }
 }
