b97793fc5a03c16c24080b8cba07f1351c9cabf5
dnjawm19 <dnjawm19@naver.com>
2024-05-29 15:18:23 +0900

    feat: UserControllerTestData 추가

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'dnjawm19@naver.com', '무관심', '경기도', 'aaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'dnjawm1995@naver.com', '무관심좋아', '경기도', 'aaab', 'PENDING', 0);
